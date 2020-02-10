b9ecb0fd6a32e4a454a8186a03b32bcb549b651a
Kamil Hajduk <kenny@macbook-pro-kamil.home>
2020-02-10 10:30:48 +0100

    Change colors and visual

--- src/App.js
+++ src/App.js
@@ -1,13 +1,178 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
-import MyComponent from './components/MyComponent/MyComponent';
 import './index.css';
+import carImage from './assets/images/car.png';
+import planeImage from './assets/images/plane.png';
 
-const App = () => (
-        <div>
-                <MyComponent />
-                <ListWrapper />
-        </div>
-);
+const initialStateItems = [
+        {
+            image: planeImage,
+            date: 'December 2019',
+            departure: 'Warsaw',
+            arrival: 'Katowice',
+            country: 'Poland',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'August 2018',
+            departure: 'Warsaw',
+            arrival: 'Nosy Be',
+            country: 'Madagascar',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'August 2017',
+            departure: 'Katowice',
+            arrival: 'Milano',
+            country: 'Italy',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'May 2017',
+            departure: 'Katowice',
+            arrival: 'Dubai',
+            country: 'United Arab Emirates',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'March 2017',
+            departure: 'Vienna',
+            arrival: 'Budapest',
+            country: 'Hungary',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'March 2017',
+            departure: 'Warsaw',
+            arrival: 'Vienna',
+            country: 'Austria',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'September 2016',
+            departure: 'Singapore',
+            arrival: 'Phuket',
+            country: 'Thailand',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'September 2016',
+            departure: 'Bangkok',
+            arrival: 'Singapore',
+            country: 'Singapore',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'September 2016',
+            departure: 'Warsaw',
+            arrival: 'Bangkok',
+            country: 'Thailand',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'April 2016',
+            departure: 'Cracow',
+            arrival: 'Oslo',
+            country: 'Norway',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'February 2016',
+            departure: 'Katowice',
+            arrival: 'Amsterdam',
+            country: 'Netherlands',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'September 2015',
+            departure: 'Warsaw',
+            arrival: 'Ibiza',
+            country: 'Spain',
+            link: '',
+        },
+        {
+            image: planeImage,
+            date: 'October 2014',
+            departure: 'Katowice',
+            arrival: 'Sousse',
+            country: 'Tunisia',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'August 2014',
+            departure: 'Katowice',
+            arrival: 'Ostrava',
+            country: 'Czech Republic',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'February 2012',
+            departure: 'Katowice',
+            arrival: 'Berlin',
+            country: 'Germany',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'July 2010',
+            departure: 'Cannes',
+            arrival: 'Monte Carlo',
+            country: 'Monaco',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'July 2010',
+            departure: 'Venice',
+            arrival: 'Cannes',
+            country: 'France',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'July 2010',
+            departure: 'Katowice',
+            arrival: 'Venice',
+            country: 'Italy',
+            link: '',
+        },
+        {
+            image: carImage,
+            date: 'July 2008',
+            departure: 'Katowice',
+            arrival: 'Varna',
+            country: 'Bulgaria',
+            link: '',
+        }
+    ];
+
+class App extends React.Component {
+        state = {
+                items: [...initialStateItems],
+        }
+
+        render() {
+                return(
+                        <div>
+                                <ListWrapper 
+                                        items={this.state.items} 
+                                />
+                        </div>
+                )
+        }
+}
 
 export default App;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
-import { travels } from '../../data/travels';
 
-const ListWrapper = () => (
+const ListWrapper = (props) => (
     <ul className="listWrapper_wrapper">
-        {travels.map(item => (
+        {props.items.map(item => (
             <ListItem key={item.arrival} {...item} />
         ))}
     </ul>
